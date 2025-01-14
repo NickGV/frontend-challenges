d981d173e9126232d1d5b870c217ff624eb68365
Nick <nickgomvelez@gmail.com>
2025-01-14 10:56:59 -0500

    feat: update modal functionality and styles for reward selection

--- JUNIOR/crowdfunding-product-page-main/script.js
+++ JUNIOR/crowdfunding-product-page-main/script.js
@@ -1,6 +1,14 @@
-const rewards = document.querySelectorAll(".reward");
+document.addEventListener("DOMContentLoaded", function() {
+  const rewardBtn = document.querySelectorAll("#reward-btn");
+  rewardBtn.forEach(btn => {
+    btn.addEventListener("click", function() {
+      $('#pledgeModal').modal('hide');
+      $('#completeModal').modal('show');
+    });
+  });
+});
 
-console.log(rewards)
+const rewards = document.querySelectorAll(".reward");
 
 rewards.forEach((reward) => {
   reward.addEventListener("click", (event) => {
@@ -8,5 +16,28 @@ rewards.forEach((reward) => {
       reward.classList.remove("active");
     });
     reward.classList.add("active");
+    console.log(reward.querySelector("#reward-btn"));
+    const rewardBtn = reward.querySelector("#reward-btn");
+    if (rewardBtn) {
+      console.log(rewardBtn);
+      rewardBtn.addEventListener("click", (event) => {
+        console.log("click");
+        event.stopPropagation();
+        closeModal();
+        showComplete();
+      });
+    }
   });
 });
+
+function showComplete() {
+  const complete = document.querySelector(".complete-modal");
+  complete.classList.add("active");
+}
+
+function closeModal() {
+  console.log("close");
+  const modal = document.querySelector("#pledgeModal");
+  modal.classList.remove("show");
+  modal.style = "display:none";
+}
